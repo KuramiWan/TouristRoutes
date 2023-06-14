8b3a9644a8721c9f8a1f2ee62620c5bec2b6806d
kurami <kurami.noya@gmail.com>
2023-06-14 14:40:51 +0800

    feat： 添加产品

---
+++ jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/product/entity/Product.java
@@ -0,0 +1,78 @@
+package org.jeecg.modules.product.entity;
+
+import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+import java.math.BigDecimal;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.jeecgframework.poi.excel.annotation.Excel;
+import org.jeecg.common.aspect.annotation.Dict;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * @Description: 旅游产品表
+ * @Author: jeecg-boot
+ * @Date:   2023-06-14
+ * @Version: V1.0
+ */
+@Data
+@TableName("product")
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = false)
+@ApiModel(value="product对象", description="旅游产品表")
+public class Product implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+	/**主键*/
+	@TableId(type = IdType.ASSIGN_ID)
+    @ApiModelProperty(value = "主键")
+    private java.lang.String id;
+	/**创建人*/
+    @ApiModelProperty(value = "创建人")
+    private java.lang.String createBy;
+	/**创建日期*/
+	@JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty(value = "创建日期")
+    private java.util.Date createTime;
+	/**更新人*/
+    @ApiModelProperty(value = "更新人")
+    private java.lang.String updateBy;
+	/**更新日期*/
+	@JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty(value = "更新日期")
+    private java.util.Date updateTime;
+	/**所属部门*/
+    @ApiModelProperty(value = "所属部门")
+    private java.lang.String sysOrgCode;
+	/**标题*/
+	@Excel(name = "标题", width = 15)
+    @ApiModelProperty(value = "标题")
+    private java.lang.String title;
+	/**描述*/
+	@Excel(name = "描述", width = 15)
+    @ApiModelProperty(value = "描述")
+    private java.lang.String productDec;
+	/**出发地*/
+	@Excel(name = "出发地", width = 15)
+    @ApiModelProperty(value = "出发地")
+    private java.lang.String departure;
+	/**标签*/
+	@Excel(name = "标签", width = 15)
+    @ApiModelProperty(value = "标签")
+    private java.lang.String tags;
+	/**海报*/
+	@Excel(name = "海报", width = 15)
+    @ApiModelProperty(value = "海报")
+    private java.lang.String img;
+}
---
+++ jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/product/service/impl/ProductServiceImpl.java
@@ -0,0 +1,19 @@
+package org.jeecg.modules.product.service.impl;
+
+import org.jeecg.modules.product.entity.Product;
+import org.jeecg.modules.product.mapper.ProductMapper;
+import org.jeecg.modules.product.service.IProductService;
+import org.springframework.stereotype.Service;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+/**
+ * @Description: 旅游产品表
+ * @Author: jeecg-boot
+ * @Date:   2023-06-14
+ * @Version: V1.0
+ */
+@Service
+public class ProductServiceImpl extends ServiceImpl<ProductMapper, Product> implements IProductService {
+
+}
