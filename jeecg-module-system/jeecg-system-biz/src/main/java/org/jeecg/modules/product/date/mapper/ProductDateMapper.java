6b99a9e3b289a328e418948cd8b567593fe560a5
Mingyu <1915290352@qq.com>
2023-07-12 21:46:45 +0800

    后端重构

--- jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/product/date/mapper/ProductDateMapper.java
+++
@@ -1,9 +0,0 @@
-package org.jeecg.modules.product.date.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-import org.jeecg.modules.product.date.pojo.ProductDate;
-
-@Mapper
-public interface ProductDateMapper extends BaseMapper<ProductDate> {
-}
--- jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/product/tags/mapper/TagsMapper.java
+++
@@ -1,9 +0,0 @@
-package org.jeecg.modules.product.tags.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-import org.jeecg.modules.product.tags.pojo.ProductTag;
-
-@Mapper
-public interface TagsMapper extends BaseMapper<ProductTag> {
-}
