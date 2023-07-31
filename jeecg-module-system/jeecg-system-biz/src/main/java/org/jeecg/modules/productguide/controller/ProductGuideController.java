8bd4b0cd1c0fc2c6301665d1aa1384dc5942e416
BooleanChar12 <2507260744@qq.com>
2023-07-31 16:56:07 +0800

    feat:根据产品id查询导游接口

--- jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/productguide/controller/ProductGuideController.java
+++ jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/productguide/controller/ProductGuideController.java
@@ -9,9 +9,13 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import org.jeecg.common.api.vo.Result;
 import org.jeecg.common.system.query.QueryGenerator;
 import org.jeecg.common.util.oConvertUtils;
+import org.jeecg.modules.guide.entity.TouristGuide;
+import org.jeecg.modules.guide.service.ITouristGuideService;
 import org.jeecg.modules.productguide.entity.ProductGuide;
 import org.jeecg.modules.productguide.service.IProductGuideService;
 
@@ -37,139 +41,151 @@ import io.swagger.annotations.ApiOperation;
 import org.jeecg.common.aspect.annotation.AutoLog;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 
- /**
+/**
  * @Description: 产品导游关系表
  * @Author: jeecg-boot
- * @Date:   2023-07-13
+ * @Date: 2023-07-13
  * @Version: V1.0
  */
-@Api(tags="产品导游关系表")
+@Api(tags = "产品导游关系表")
 @RestController
 @RequestMapping("/productguide/productGuide")
 @Slf4j
 public class ProductGuideController extends JeecgController<ProductGuide, IProductGuideService> {
-	@Autowired
-	private IProductGuideService productGuideService;
-	
-	/**
-	 * 分页列表查询
-	 *
-	 * @param productGuide
-	 * @param pageNo
-	 * @param pageSize
-	 * @param req
-	 * @return
-	 */
-	//@AutoLog(value = "产品导游关系表-分页列表查询")
-	@ApiOperation(value="产品导游关系表-分页列表查询", notes="产品导游关系表-分页列表查询")
-	@GetMapping(value = "/list")
-	public Result<IPage<ProductGuide>> queryPageList(ProductGuide productGuide,
-								   @RequestParam(name="pageNo", defaultValue="1") Integer pageNo,
-								   @RequestParam(name="pageSize", defaultValue="10") Integer pageSize,
-								   HttpServletRequest req) {
-		QueryWrapper<ProductGuide> queryWrapper = QueryGenerator.initQueryWrapper(productGuide, req.getParameterMap());
-		Page<ProductGuide> page = new Page<ProductGuide>(pageNo, pageSize);
-		IPage<ProductGuide> pageList = productGuideService.page(page, queryWrapper);
-		return Result.OK(pageList);
-	}
-	
-	/**
-	 *   添加
-	 *
-	 * @param productGuide
-	 * @return
-	 */
-	@AutoLog(value = "产品导游关系表-添加")
-	@ApiOperation(value="产品导游关系表-添加", notes="产品导游关系表-添加")
-	@RequiresPermissions("productguide:product_guide:add")
-	@PostMapping(value = "/add")
-	public Result<String> add(@RequestBody ProductGuide productGuide) {
-		productGuideService.save(productGuide);
-		return Result.OK("添加成功！");
-	}
-	
-	/**
-	 *  编辑
-	 *
-	 * @param productGuide
-	 * @return
-	 */
-	@AutoLog(value = "产品导游关系表-编辑")
-	@ApiOperation(value="产品导游关系表-编辑", notes="产品导游关系表-编辑")
-	@RequiresPermissions("productguide:product_guide:edit")
-	@RequestMapping(value = "/edit", method = {RequestMethod.PUT,RequestMethod.POST})
-	public Result<String> edit(@RequestBody ProductGuide productGuide) {
-		productGuideService.updateById(productGuide);
-		return Result.OK("编辑成功!");
-	}
-	
-	/**
-	 *   通过id删除
-	 *
-	 * @param id
-	 * @return
-	 */
-	@AutoLog(value = "产品导游关系表-通过id删除")
-	@ApiOperation(value="产品导游关系表-通过id删除", notes="产品导游关系表-通过id删除")
-	@RequiresPermissions("productguide:product_guide:delete")
-	@DeleteMapping(value = "/delete")
-	public Result<String> delete(@RequestParam(name="id",required=true) String id) {
-		productGuideService.removeById(id);
-		return Result.OK("删除成功!");
-	}
-	
-	/**
-	 *  批量删除
-	 *
-	 * @param ids
-	 * @return
-	 */
-	@AutoLog(value = "产品导游关系表-批量删除")
-	@ApiOperation(value="产品导游关系表-批量删除", notes="产品导游关系表-批量删除")
-	@RequiresPermissions("productguide:product_guide:deleteBatch")
-	@DeleteMapping(value = "/deleteBatch")
-	public Result<String> deleteBatch(@RequestParam(name="ids",required=true) String ids) {
-		this.productGuideService.removeByIds(Arrays.asList(ids.split(",")));
-		return Result.OK("批量删除成功!");
-	}
-	
-	/**
-	 * 通过id查询
-	 *
-	 * @param id
-	 * @return
-	 */
-	//@AutoLog(value = "产品导游关系表-通过id查询")
-	@ApiOperation(value="产品导游关系表-通过id查询", notes="产品导游关系表-通过id查询")
-	@GetMapping(value = "/queryById")
-	public Result<ProductGuide> queryById(@RequestParam(name="id",required=true) String id) {
-		ProductGuide productGuide = productGuideService.getById(id);
-		if(productGuide==null) {
-			return Result.error("未找到对应数据");
-		}
-		return Result.OK(productGuide);
-	}
+    @Autowired
+    private IProductGuideService productGuideService;
+
+    @Autowired
+    private ITouristGuideService touristGuideService;
 
     /**
-    * 导出excel
-    *
-    * @param request
-    * @param productGuide
-    */
-    @RequiresPermissions("productguide:product_guide:exportXls")
+     * 分页列表查询
+     *
+     * @param productGuide
+     * @param pageNo
+     * @param pageSize
+     * @param req
+     * @return
+     */
+    //@AutoLog(value = "产品导游关系表-分页列表查询")
+    @ApiOperation(value = "产品导游关系表-分页列表查询", notes = "产品导游关系表-分页列表查询")
+    @GetMapping(value = "/list")
+    public Result<IPage<ProductGuide>> queryPageList(ProductGuide productGuide,
+                                                     @RequestParam(name = "pageNo", defaultValue = "1") Integer pageNo,
+                                                     @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize,
+                                                     HttpServletRequest req) {
+        QueryWrapper<ProductGuide> queryWrapper = QueryGenerator.initQueryWrapper(productGuide, req.getParameterMap());
+        Page<ProductGuide> page = new Page<ProductGuide>(pageNo, pageSize);
+        IPage<ProductGuide> pageList = productGuideService.page(page, queryWrapper);
+        return Result.OK(pageList);
+    }
+
+    /**
+     * 添加
+     *
+     * @param productGuide
+     * @return
+     */
+    @AutoLog(value = "产品导游关系表-添加")
+    @ApiOperation(value = "产品导游关系表-添加", notes = "产品导游关系表-添加")
+    @PostMapping(value = "/add")
+    public Result<String> add(@RequestBody ProductGuide productGuide) {
+        productGuideService.save(productGuide);
+        return Result.OK("添加成功！");
+    }
+
+    /**
+     * 编辑
+     *
+     * @param productGuide
+     * @return
+     */
+    @AutoLog(value = "产品导游关系表-编辑")
+    @ApiOperation(value = "产品导游关系表-编辑", notes = "产品导游关系表-编辑")
+    @RequestMapping(value = "/edit", method = {RequestMethod.PUT, RequestMethod.POST})
+    public Result<String> edit(@RequestBody ProductGuide productGuide) {
+        productGuideService.updateById(productGuide);
+        return Result.OK("编辑成功!");
+    }
+
+    /**
+     * 通过id删除
+     *
+     * @param id
+     * @return
+     */
+    @AutoLog(value = "产品导游关系表-通过id删除")
+    @ApiOperation(value = "产品导游关系表-通过id删除", notes = "产品导游关系表-通过id删除")
+    @DeleteMapping(value = "/delete")
+    public Result<String> delete(@RequestParam(name = "id", required = true) String id) {
+        productGuideService.removeById(id);
+        return Result.OK("删除成功!");
+    }
+
+    /**
+     * 批量删除
+     *
+     * @param ids
+     * @return
+     */
+    @AutoLog(value = "产品导游关系表-批量删除")
+    @ApiOperation(value = "产品导游关系表-批量删除", notes = "产品导游关系表-批量删除")
+    @DeleteMapping(value = "/deleteBatch")
+    public Result<String> deleteBatch(@RequestParam(name = "ids", required = true) String ids) {
+        this.productGuideService.removeByIds(Arrays.asList(ids.split(",")));
+        return Result.OK("批量删除成功!");
+    }
+
+    /**
+     * 通过id查询
+     *
+     * @param id
+     * @return
+     */
+    //@AutoLog(value = "产品导游关系表-通过id查询")
+    @ApiOperation(value = "产品导游关系表-通过id查询", notes = "产品导游关系表-通过id查询")
+    @GetMapping(value = "/queryById")
+    public Result<ProductGuide> queryById(@RequestParam(name = "id", required = true) String id) {
+        ProductGuide productGuide = productGuideService.getById(id);
+        if (productGuide == null) {
+            return Result.error("未找到对应数据");
+        }
+        return Result.OK(productGuide);
+    }
+
+    /**
+     * 通过产品id查询导游信息
+     *
+     * @param productId
+     * @return
+     */
+    //@AutoLog(value = "产品导游关系表-通过id查询")
+    @ApiOperation(value = "产品导游关系表-通过产品id查询导游信息", notes = "产品导游关系表-通过产品id查询导游信息")
+    @GetMapping(value = "/queryByProId")
+    public Result<List<TouristGuide>> queryByProId(@RequestParam(name = "productId", required = true) String productId) {
+        List<ProductGuide> list = productGuideService.list(new LambdaQueryWrapper<ProductGuide>().eq(ProductGuide::getProductId, productId));
+        List<String> ids = list.stream().map(ProductGuide::getGuideId).collect(Collectors.toList());
+        return Result.OK(touristGuideService.listByIds(ids));
+    }
+
+    /**
+     * 导出excel
+     *
+     * @param request
+     * @param productGuide
+     */
     @RequestMapping(value = "/exportXls")
     public ModelAndView exportXls(HttpServletRequest request, ProductGuide productGuide) {
         return super.exportXls(request, productGuide, ProductGuide.class, "产品导游关系表");
     }
 
     /**
-      * 通过excel导入数据
-    *
-    * @param request
-    * @param response
-    * @return
-    */
-    @RequiresPermissions("productguide:product_guide:importExcel")
+     * 通过excel导入数据
+     *
+     * @param request
+     * @param response
+     * @return
+     */
     @RequestMapping(value = "/importExcel", method = RequestMethod.POST)
     public Result<?> importExcel(HttpServletRequest request, HttpServletResponse response) {
         return super.importExcel(request, response, ProductGuide.class);
