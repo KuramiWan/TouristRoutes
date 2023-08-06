9de864e0ba8b22771c6f3e18648f72bf044c5ba8
BooleanChar12 <2507260744@qq.com>
2023-08-06 21:10:43 +0800

    feat:导游管理接口

--- jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/guide/controller/TouristGuideController.java
+++ jeecg-module-system/jeecg-system-biz/src/main/java/org/jeecg/modules/guide/controller/TouristGuideController.java
@@ -1,9 +1,6 @@
 package org.jeecg.modules.guide.controller;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
@@ -12,9 +9,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import org.jeecg.common.api.vo.Result;
 import org.jeecg.common.system.query.QueryGenerator;
 import org.jeecg.common.util.oConvertUtils;
+import org.jeecg.common.util.oss.OssBootUtil;
 import org.jeecg.modules.guide.dto.UserAndLeader;
 import org.jeecg.modules.guide.entity.TouristGuide;
 import org.jeecg.modules.guide.entity.UserLeaderLike;
@@ -67,12 +66,12 @@ public class TouristGuideController extends JeecgController<TouristGuide, ITouri
     private IUserLeaderLikeService userLeaderLikeService;
 
     @ApiOperation(value = "导游表-增加点赞数量", notes = "导游表-增加点赞数量")
-    @PostMapping (value = "/addLikeNum")
+    @PostMapping(value = "/addLikeNum")
     @Transactional(rollbackFor = Exception.class)
     public Result<Integer> addLikeNum(@RequestBody UserAndLeader userAndLeader) throws Exception {
         TouristGuide touristGuide = touristGuideService.getById(userAndLeader.getGuideId());
         Integer likeNum = touristGuide.getLikeNum();
-        touristGuide.setLikeNum(likeNum+1);
+        touristGuide.setLikeNum(likeNum + 1);
         // 加入到用户点赞导游表中
         UserLeaderLike userLeaderLike =
                 new UserLeaderLike()
@@ -80,12 +79,12 @@ public class TouristGuideController extends JeecgController<TouristGuide, ITouri
                         .setUserId(userAndLeader.getUserId());
         LambdaQueryWrapper<UserLeaderLike> queryWrapper =
                 new LambdaQueryWrapper<UserLeaderLike>()
-                        .eq(UserLeaderLike::getUserId,userAndLeader.getUserId())
-                        .eq(UserLeaderLike::getLeaderId,userAndLeader.getGuideId());
+                        .eq(UserLeaderLike::getUserId, userAndLeader.getUserId())
+                        .eq(UserLeaderLike::getLeaderId, userAndLeader.getGuideId());
         long count = userLeaderLikeService.count(queryWrapper);
         touristGuideService.updateById(touristGuide);
         userLeaderLikeService.save(userLeaderLike);
-        if(count>=1) throw new DuplicateKeyException("点赞失败");
+        if (count >= 1) throw new DuplicateKeyException("点赞失败");
         return Result.OK(touristGuide.getLikeNum());
     }
 
@@ -95,15 +94,15 @@ public class TouristGuideController extends JeecgController<TouristGuide, ITouri
     public Result<Integer> subLikeNum(@RequestBody UserAndLeader userAndLeader) throws Exception {
         TouristGuide touristGuide = touristGuideService.getById(userAndLeader.getGuideId());
         Integer likeNum = touristGuide.getLikeNum();
-        touristGuide.setLikeNum(likeNum-1);
+        touristGuide.setLikeNum(likeNum - 1);
         //从点赞表中删除这条记录
         LambdaQueryWrapper<UserLeaderLike> queryWrapper =
                 new LambdaQueryWrapper<UserLeaderLike>()
-                        .eq(UserLeaderLike::getUserId,userAndLeader.getUserId())
-                        .eq(UserLeaderLike::getLeaderId,userAndLeader.getGuideId());
+                        .eq(UserLeaderLike::getUserId, userAndLeader.getUserId())
+                        .eq(UserLeaderLike::getLeaderId, userAndLeader.getGuideId());
         boolean update = touristGuideService.updateById(touristGuide);
         boolean remove = userLeaderLikeService.remove(queryWrapper);
-        if(!(update && remove)) throw new DataRetrievalFailureException("取消失败");
+        if (!(update && remove)) throw new DataRetrievalFailureException("取消失败");
         return Result.OK(touristGuide.getLikeNum());
     }
 
@@ -120,29 +119,66 @@ public class TouristGuideController extends JeecgController<TouristGuide, ITouri
     @ApiOperation(value = "导游表-分页列表查询", notes = "导游表-分页列表查询")
     @GetMapping(value = "/list")
     public Result<IPage<TouristGuide>> getPageList(TouristGuide touristGuide,
-                                                   @RequestParam(name = "userId",required = false) String userId,
+                                                   @RequestParam(name = "userId", required = false) String userId,
                                                    @RequestParam(name = "pageNo", defaultValue = "1") Integer pageNo,
                                                    @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize,
                                                    HttpServletRequest req) {
-        Page<TouristGuide> page = new Page<>(pageNo,pageSize);
+        Page<TouristGuide> page = new Page<>(pageNo, pageSize);
         LambdaQueryWrapper<TouristGuide> queryWrapper =
                 new LambdaQueryWrapper<TouristGuide>().orderByDesc(TouristGuide::getLikeNum);
         IPage pageList = touristGuideService.page(page, queryWrapper);
         List<TouristGuide> records = pageList.getRecords();
         ArrayList<WaterFallGuide> waterFallGuides = new ArrayList<>();
         records.forEach(
-                record->{
+                record -> {
                     WaterFallGuide waterFallGuide = new WaterFallGuide();
-                    BeanUtils.copyProperties(record,waterFallGuide);
+                    BeanUtils.copyProperties(record, waterFallGuide);
                     LambdaQueryWrapper<UserLeaderLike> starQueryWrapper =
                             new LambdaQueryWrapper<UserLeaderLike>()
-                                    .eq(UserLeaderLike::getUserId,userId)
-                                    .eq(UserLeaderLike::getLeaderId,record.getId());
+                                    .eq(UserLeaderLike::getUserId, userId)
+                                    .eq(UserLeaderLike::getLeaderId, record.getId());
                     long count = userLeaderLikeService.count(starQueryWrapper);
                     waterFallGuide.setValue(count);
                     waterFallGuides.add(waterFallGuide);
                 });
         pageList.setRecords(waterFallGuides);
+        return Result.OK(pageList);
+    }
+
+
+    /**
+     * 分页列表查询
+     *
+     * @param pageNo
+     * @param pageSize
+     * @return
+     */
+    //@AutoLog(value = "导游表-分页列表查询")
+    @ApiOperation(value = "导游表-分页列表查询", notes = "导游表-分页列表查询")
+    @GetMapping(value = "/listAll")
+    public Result<IPage<TouristGuide>> getPageListAll(@RequestParam(name = "pageNo", defaultValue = "1") Integer pageNo, @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize) {
+        Page<TouristGuide> page = new Page<>(pageNo, pageSize);
+        LambdaQueryWrapper<TouristGuide> queryWrapper =
+                new LambdaQueryWrapper<TouristGuide>().orderByDesc(TouristGuide::getCreateTime);
+        IPage<TouristGuide> pageList = touristGuideService.page(page, queryWrapper);
+        return Result.OK(pageList);
+    }
+
+    /**
+     * 分页列表根据姓名模糊查询
+     *
+     * @param pageNo
+     * @param pageSize
+     * @return
+     */
+    //@AutoLog(value = "导游表-分页列表根据姓名模糊查询")
+    @ApiOperation(value = "导游表-分页列表根据姓名模糊查询", notes = "导游表-分页列表根据姓名模糊查询")
+    @GetMapping(value = "/listByName")
+    public Result<IPage<TouristGuide>> listByName(@RequestParam(name = "name", defaultValue = "1") String name, @RequestParam(name = "pageNo", defaultValue = "1") Integer pageNo, @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize) {
+        Page<TouristGuide> page = new Page<>(pageNo, pageSize);
+        LambdaQueryWrapper<TouristGuide> queryWrapper =
+                new LambdaQueryWrapper<TouristGuide>().like(TouristGuide::getName, name).orderByDesc(TouristGuide::getCreateTime);
+        IPage<TouristGuide> pageList = touristGuideService.page(page, queryWrapper);
         return Result.OK(pageList);
     }
 
@@ -157,6 +193,27 @@ public class TouristGuideController extends JeecgController<TouristGuide, ITouri
     @PostMapping(value = "/add")
     public Result<String> add(@RequestBody TouristGuide touristGuide) {
         touristGuideService.save(touristGuide);
+        return Result.OK("添加成功！");
+    }
+
+    /**
+     * 添加or编辑
+     *
+     * @param touristGuide
+     * @return
+     */
+    @AutoLog(value = "导游表-添加or编辑")
+    @ApiOperation(value = "导游表-添加or编辑", notes = "导游表-添加or编辑")
+    @PostMapping(value = "/addOrEdit")
+    public Result<String> addOrEdit(@RequestBody TouristGuide touristGuide) {
+        touristGuide.setPageImg(touristGuide.getAvatar());
+        //根据传过来的touristGuide中是否有id来区分
+        if (StringUtils.isNotEmpty(touristGuide.getId())) {
+            touristGuideService.updateById(touristGuide);
+            return Result.OK("编辑成功！");
+        } else {
+            touristGuideService.save(touristGuide);
+        }
         return Result.OK("添加成功！");
     }
 
@@ -217,6 +274,34 @@ public class TouristGuideController extends JeecgController<TouristGuide, ITouri
             return Result.error("未找到对应数据");
         }
         return Result.OK(touristGuide);
+    }
+
+    @AutoLog(value = "某个产品某一天的所有任务-上传图床返回url")
+    @ApiOperation(value = "某个产品某一天的所有任务-上传图床返回url", notes = "某个产品某一天的所有任务-上传图床返回url")
+    @PostMapping(value = "/uploadGuideImg")
+    public Result<List<String>> uploadGuideImg(@RequestBody Map<String, String> map) {
+        ArrayList<String> list = new ArrayList<>();
+        String base64Img = map.get("base64Img");
+        //上传图床并更新数据库中的图片字段
+        list.add(uploadImg(base64Img));
+        return Result.OK(list);
+    }
+
+    public String uploadImg(String base64Img) {
+        try {
+            // 将Base64数据转换为字节数组
+            byte[] pageImg = Base64.getDecoder().decode(base64Img);
+            String fileDir1 = "suixinyou-wx-client/pages-tourGuide/"; // 文件保存目录，根据实际情况调整
+            String fileUrl1 = OssBootUtil.upload(new Date().toString(), pageImg, fileDir1);
+
+            if (fileUrl1 == null) return "上传失败";
+
+            return fileUrl1;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "上传失败";
+        }
     }
 
 
